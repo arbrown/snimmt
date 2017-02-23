0242f54094d2e016092187994e64ce6b1d009228
Drew Brown <drew@drewbrown.me>
2017-02-22 21:56:56 -0700

    Initial Commit
    Adding basic files for Snimmt project.

---
+++ Snimmt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snimmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
